6bdfce900f355f49d2e042f1c0426c96342216c9
Andrew Lee <drew@Andrew-Lees-MacBook-Pro.local>
2011-03-03 14:48:12 -0800

    FLX-214 grantee portal

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -21,7 +21,7 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
       flash[:notice] = "Login successful!"
-      redirect_back_or_default dashboard_index_path
+      redirect_back_or_default  @user_session.user.is_grantee? ? grantee_portal_index_path : dashboard_index_path
     else
       render :action => :new
     end
