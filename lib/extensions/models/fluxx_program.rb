6b68fbed580bfd61d9ee3f18fc593ce45f334d1f
Eric Hansen <ehansen486@gmail.com>
2011-03-03 23:15:33 -0800

    FLX-533 FIP (FR-1103-13813) no longer showing up in the system - despite not being deleted

--- lib/extensions/models/fluxx_program.rb
+++ lib/extensions/models/fluxx_program.rb
@@ -150,7 +150,7 @@ module FluxxProgram
       
       program_ids = programs.compact.flatten.map &:id
       
-      user_query = User.joins(:role_users => :role).where({:test_user_flag => 0, :role_users => {:roleable_id => program_ids, :roles => {:roleable_type => self.class.name}}})
+      user_query = User.joins(:role_users => :role).order('first_name asc, last_name asc').where({:test_user_flag => 0, :role_users => {:roleable_id => program_ids, :roles => {:roleable_type => self.class.name}}})
       user_query = user_query.where({:role_users => {:roles => {:name => role_name}}}) if role_name
       user_query.group("users.id").compact
     end
