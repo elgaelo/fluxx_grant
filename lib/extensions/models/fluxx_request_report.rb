94cf5ff24bf95e49aae5cc4bee06620fcc54bc67
Eric Hansen <ehansen486@gmail.com>
2011-04-13 09:17:30 -0700

    fix issue with transactions and reports being related only to grants and not to fips

--- lib/extensions/models/fluxx_request_report.rb
+++ lib/extensions/models/fluxx_request_report.rb
@@ -5,7 +5,7 @@ module FluxxRequestReport
 
   def self.included(base)
     base.belongs_to :request
-    base.belongs_to :grant, :class_name => 'GrantRequest', :foreign_key => 'request_id', :conditions => {:granted => true}
+    base.belongs_to :grant, :class_name => 'Request', :foreign_key => 'request_id', :conditions => {:granted => true}
     
     base.belongs_to :created_by, :class_name => 'User', :foreign_key => 'created_by_id'
     base.belongs_to :updated_by, :class_name => 'User', :foreign_key => 'updated_by_id'
