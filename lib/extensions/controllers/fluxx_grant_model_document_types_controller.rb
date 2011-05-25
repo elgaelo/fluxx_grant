bcf99d1068624ae0b1cf9e66b4d272dca227c087
Eric Hansen <ehansen486@gmail.com>
2011-05-25 15:35:17 -0700

    refactor a bit how the document type is loaded

--- lib/extensions/controllers/fluxx_grant_model_document_types_controller.rb
+++ lib/extensions/controllers/fluxx_grant_model_document_types_controller.rb
@@ -3,13 +3,12 @@ module FluxxGrantModelDocumentTypesController
     base.send :include, FluxxModelDocumentTypesController
     base.insta_index ModelDocumentType do |insta|
       insta.pre do |controller_dsl|
+        # Note if we are not loading up a request don't bother to preload, let the usual loader do its work
         if params[:model_id] && params[:model_type]
           klass = Kernel.const_get params[:model_type] rescue nil
           if klass && klass.extract_classes(klass).any?{|cur_klass| cur_klass == Request} && (request_model = klass.find(params[:model_id]))
-            self.pre_models = ModelDocumentType.where(:model_type => klass.name).where(['if(program_id is not null, program_id = ?, true) AND 
-              if(sub_program_id is not null, program_id = ?, true) AND 
-                if(initiative_id is not null, initiative_id = ?, true) AND 
-                  if(sub_initiative_id is not null, initiative_id = ?, true)',request_model.program_id, request_model.sub_program_id, request_model.initiative_id, request_model.sub_initiative_id]).order('name asc').all
+            self.pre_models = ModelDocumentType.load_by_class(klass, :program_id => request_model.program_id, :sub_program_id => request_model.sub_program_id, 
+              :initiative_id => request_model.initiative_id, :sub_initiative_id => request_model.sub_initiative_id)
           end
         end
           
