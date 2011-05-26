138a97390ed3082cba5a7ee7d224294c045c88e6
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2011-05-26 16:35:17 -0300

    added mocha

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require File.join(File.dirname(__FILE__), 'blueprint')
 require "rails/test_help"
 
+require 'mocha'
+
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
