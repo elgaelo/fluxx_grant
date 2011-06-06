c3c207342246754e1c601864ba3e9a845ad50e73
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2011-06-06 16:48:18 -0300

    fixed hoptoad

--- lib/fluxx_grant.rb
+++ lib/fluxx_grant.rb
@@ -2,6 +2,7 @@ require "rails"
 require "fluxx_crm"
 
 require "delayed_job"
+require "hoptoad_notifier"
 
 # Some classes need to be required before or after; put those in these lists
 GRANT_EXTENSION_CLASSES_TO_PRELOAD = []
