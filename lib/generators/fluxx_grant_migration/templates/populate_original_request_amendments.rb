3ce37e1108a73c9c72da2c90cc87f5ec02791822
Chris Kowalik <dev+nu7hatch@cuboxsa.com>
2011-06-15 18:09:21 -0300

    fixed amendments migrations

--- lib/generators/fluxx_grant_migration/templates/populate_original_request_amendments.rb
+++ lib/generators/fluxx_grant_migration/templates/populate_original_request_amendments.rb
@@ -1,34 +1,33 @@
 class FluxxGrantPopulateOriginalRequestAmendments < ActiveRecord::Migration
-  def self.fill_in_amendment(amendment, changes)
-    amendment.amount_recommended = changes["amount_recommended"] if changes["amount_recomended"]
-          
-    unless (Fluxx.config(:dont_use_duration_in_requests) == "1")
-      amendment.duration = changes["duration_in_months"] if changes["duration_in_months"]
-    else
-      amendment.start_at = changes["grant_begins_at"] if changes["grant_begins_at"]
-      amendment.end_at = changes["grant_closed_at"] if changes["grant_closed_at"]
-    end
+  def self.normalize_attributes(attrs)
+    return {} if attrs.is_a?(String)
 
-    return !!(changes["grant_begins_at"] || changes["grant_closed_at"] || 
-      changes["duration_in_months"] || changes["amount_recommended"])
+    attrs["start_date"] ||= attrs["grant_begins_at"]
+    attrs["end_date"]   ||= attrs["grant_closed_at"]
+    attrs["duration"]   ||= attrs["duration_in_months"]
+    
+    attrs.reject { |k,v| v.nil? or not %w[start_date end_date duration amount_recommended].include?(k.to_s) }
   end
 
   def self.up
     RequestAmendment.delete_all
     Request.where(:granted => 1).each { |request|
       original = request.request_amendments.build(:original => true)
-      fill_in_amendment(original, request.attributes);
+      original.attributes = normalize_attributes(request.attributes)
 
       request.audits.each { |audit|
         changes = audit.audit_changes
-        changes.each {|x,v| changes[x] = v.last if v && v.is_a?(Array) }
+        changes = normalize_attributes(changes)
+        changes.each { |k,v| changes[k] = v.last if v && v.is_a?(Array) }
 
-        if original.new_record?
-          fill_in_amendment(original, changes)
-          original.save if state = changes["state"] and state =~ /(granted|closed|rejected)/i
-        else
-          amendment = request.request_amendments.build(:original => false)
-          amendment.save if fill_in_amendment(amendment, changes)
+        if original.new_record? && %w[granted closed rejected].include?(changes["state"].to_s)
+          original.attributes = changes
+          original.save
+        end
+
+        unless changes.empty?
+          amendment = request.request_amendments.build(changes.merge(:original => false))
+          amendment.save
         end
       }
 
