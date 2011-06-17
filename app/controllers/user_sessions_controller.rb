17584c16378855ddcb8a1e48979c2131392edf25
Andrew Lee <drew@intramedia.net>
2011-06-16 14:14:30 -1000

    FLX-1080 LAF - Update landing page and LOI Look and Feel

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -58,7 +58,11 @@ class UserSessionsController < ApplicationController
     @user_session = UserSession.new
     respond_to do |format|
       format.html do
-        render :action => :portal, :layout => "portal_login"
+        if Fluxx.config(:hide_loi) == "1"
+          redirect_back_or_default dashboard_index_path
+        else
+          render :action => :portal, :layout => "portal_login"
+        end
       end
     end
   end
