5f4413dc176fbe6848e365ea6a60cceb536a2583
Andrew Lee <drew@intramedia.net>
2011-07-06 16:24:43 -1000

    FLX-1183 LOI Migration, fields required to create a request

--- lib/generators/fluxx_grant_migration/templates/add_request_fields_to_loi.rb
+++ lib/generators/fluxx_grant_migration/templates/add_request_fields_to_loi.rb
@@ -3,8 +3,8 @@ class FluxxGrantAddRequestFieldsToLoi < ActiveRecord::Migration
     add_column :lois, :tax_id, :string rescue nil
     add_column :lois, :amount_requested, :decimal, :scale => 2, :precision => 10 rescue nil
     add_column :lois, :program_id, :integer rescue nil
-    add_column :lois, :sub_program_id, :intefer rescue nil
-    add_column :lois, duration_in_months, :integer rescue nil
+    add_column :lois, :sub_program_id, :integer rescue nil
+    add_column :lois, :duration_in_months, :integer rescue nil
     add_column :lois, :grant_begins_at, :datetime, :null => true rescue nil
   end
 
