bb847ea3938f357f3f6740969326f64f99febb7a
Eric Hansen <ehansen486@gmail.com>
2011-07-12 07:32:47 +0200

    FLX-1177 Remove Grant End Date from EF and GTAF FIPs make it so that start/end dates are utc for amendments

--- lib/extensions/models/fluxx_request_amendment.rb
+++ lib/extensions/models/fluxx_request_amendment.rb
@@ -3,6 +3,11 @@ module FluxxRequestAmendment
 
   when_included do
     belongs_to :request, :polymorphic => true
+    
+    insta_utc do |insta|
+      insta.time_attributes = [:start_date, :end_date]
+    end
+    
   end
 
   class_methods do
